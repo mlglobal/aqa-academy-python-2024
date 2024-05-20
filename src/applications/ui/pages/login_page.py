f87006456e9040f1798872d6c0ab9dc11222890d
Maciej Leoniak <maciej.leoniak@globallogic.com>
2024-05-20 11:03:55 +0200

    login_page: try_login and creds_msg  updated

--- src/applications/ui/pages/login_page.py
+++ src/applications/ui/pages/login_page.py
@@ -5,13 +5,16 @@ class LoginPage:
     LOGIN_FLD = '//*[@id="login_field"]'
     PASSWORD_FLD = '//*[@id="password"]'
     SIGNIN_BTN = '//*[@id="login"]/div[4]/form/div/input[13]'
+    ERROR_MSG_LOCATOR = '//*[@id="js-flash-container"]/div/div' # need to be updated
     
     def __init__(self, app) -> None:
         self.app = app
 
     # user methods
-    def try_login(self,username: str, password: str):
-        pass
+    def try_login(self, username: str, password: str):
+       self.app.enter_text(self.LOGIN_FLD, username)
+       self.app.enter_text(self.PASSWORD_FLD, password)
+       self.app.wait_and_click(self.SIGNIN_BTN)
     
     def navigate_to(self):
         self.app.navigate_to(self.URL)
@@ -19,8 +22,9 @@ class LoginPage:
     # check functions
     def check_wrong_creds_message(self):
         # find error message
+        error_message = self.app.find_element(self.ERROR_MSG_LOCATOR).text
         # check if message is equal to "BLA" text
-        return True
+        return error_message == "BLA"
     
     def check_documentation_link(self):
         pass
